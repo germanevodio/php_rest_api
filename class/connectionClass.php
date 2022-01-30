ae77e95c9b7936ec1e0a59f31114d052eb14676a
german santiago <germansantiago@outlook.com>
2022-01-30 17:22:12 -0600

    created class

--- class/connectionClass.php
+++ class/connectionClass.php
@@ -3,21 +3,24 @@
 require_once "../config/dbCredentials.php";
 
 class connectionClass {
-    private $connection;
+    protected $connection;
     
-    function __construct()
+    public function __construct()
     {
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
         
         if(!$this->connection) {
             die('error connecting to database: '.mysqli_connect_error()); 
-        } else {
-            echo('connected successfull');
-        
-            // show the MySQL o MariaDB version
-            printf("server MySQL: %s\n", mysqli_get_server_info($this->connection));
-            
-            mysqli_close($this->connection);
         }
     } // end construct
+
+    /**
+     * Este método retorna la version de servidor MySQL o MariaDB
+     * 
+     * @return string
+     */
+    protected function serverVersion()
+    {
+        return mysqli_get_server_info($this->connection);
+    } // end serverVersion
 } // end class
---
+++ class/publicationsClass.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once "../models/publicationsModel.php";
+
+class publicationsClass {
+    private $name;
+    private $description;
+    private $released;
+    private $active;
+    private $userId;
+
+    public function __construct($name, $description, $released, $active, $userId)
+    {
+        $this->name        = $name;
+        $this->description = $description;
+        $this->released    = $released;
+        $this->active      = $active;
+        $this->userId   = $userId;
+    } // end __construct
+
+    /**
+     * método que almacena una publicacion
+     *
+     * @return void
+     */
+    public function storePublication() {
+        $publicationsModel = new publicationsModel();
+        
+        return $publicationsModel->insert(
+            $this->name,
+            $this->description,
+            $this->released,
+            $this->active,
+            $this->userId
+        );
+    } // end storePublication
+
+    /**
+     * método que obtiene todas las publicaciones
+     *
+     * @return void
+     */
+    public static function getPublications() {
+        $publicationsModel = new publicationsModel();
+
+        return $publicationsModel->select();
+    } // end getPublications
+
+    /**
+     * método que obtiene una publicación
+     *
+     * @param [int] $publicationId
+     * @return void
+     */
+    public static function getPublication($publicationId) {
+        $publicationsModel = new publicationsModel();
+
+        return $publicationsModel->select($publicationId);
+    } // end getPublication
+
+    /**
+     * método que actualiza una publicación
+     *
+     * @param [int] $publicationId
+     * @return void
+     */
+    public function updatePublication($publicationId) {
+        $publicationsModel = new publicationsModel();
+        
+        return $publicationsModel->update(
+            $this->name,
+            $this->description,
+            $this->released,
+            $this->active,
+            $this->userId,
+            $publicationId
+        );
+    } // end updatePublication
+
+    /**
+     * método que hace borrado lógico a una publicación
+     *
+     * @param [type] $publicationId
+     * @param [type] $userId
+     * @return void
+     */
+    public static function deletePublication($publicationId, $userId) {
+        $publicationsModel = new publicationsModel();
+
+        return $publicationsModel->delete($publicationId, $userId);
+    } // end deletePublication
+} // end class
